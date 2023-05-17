f6f8693aa08a13d7d5f45c6ae98739ca8bd5b25f
kirly <kirly@yandex.ru>
2023-05-17 10:38:59 +0300

    Cleanup

---
+++ src/Storage/IStorageClient.cs
@@ -0,0 +1,120 @@
+﻿using System.Net;
+
+namespace Storage;
+
+public interface IStorageClient : IDisposable
+{
+    /// <summary>
+    /// Builds pre-signed file url
+    /// </summary>
+    /// <remarks>This method will not check if the file exists or not: use <see cref="StorageClient.GetFileUrl"/> for check existing of file</remarks>
+    /// <param name="fileName">Name of file</param>
+    /// <param name="expiration">Time of url expiration</param>
+    /// <returns>Pre-signed URL of the file</returns>
+    string BuildFileUrl(string fileName, TimeSpan expiration);
+
+    Task<bool> CreateBucket(CancellationToken cancellation);
+
+    Task<bool> BucketExists(CancellationToken cancellation);
+
+    Task<bool> DeleteBucket(CancellationToken cancellation);
+
+    /// <summary>
+    /// Deletes the file from storage
+    /// </summary>
+    /// <param name="fileName">Name of the file to be deleted</param>
+    /// <param name="cancellation">Cancellation token</param>
+    /// <remarks>Storage sends the same response in the following cases: the file was indeed deleted, the file did not exist</remarks>
+    /// <exception cref="HttpRequestException">Connection problems or status code isn't <see cref="HttpStatusCode.NoContent"/></exception>
+    Task DeleteFile(string fileName, CancellationToken cancellation);
+
+    Task<bool> FileExists(string fileName, CancellationToken cancellation);
+
+    /// <summary>
+    /// Gets file data from the storage
+    /// </summary>
+    /// <param name="fileName">File name than will be received</param>
+    /// <param name="cancellation">Cancellation token</param>
+    /// <returns>Wrapper around <see cref="HttpResponseMessage"/> with the data of the file from storage</returns>
+    /// <exception cref="HttpRequestException">Connection problems or status code isn't isn't <see cref="HttpStatusCode.OK"/> or <see cref="HttpStatusCode.NotFound"/></exception>
+    Task<StorageFile> GetFile(string fileName, CancellationToken cancellation);
+
+    /// <summary>
+    /// Gets file stream from the storage
+    /// </summary>
+    /// <param name="fileName">File name than will be received</param>
+    /// <param name="cancellation">Cancellation token</param>
+    /// <returns>Stream from <see cref="HttpResponseMessage"/> with the data of the file from storage</returns>
+    /// <exception cref="HttpRequestException">Connection problems or status code isn't <see cref="HttpStatusCode.OK"/> or <see cref="HttpStatusCode.NotFound"/></exception>
+    Task<Stream> GetFileStream(string fileName, CancellationToken cancellation);
+
+    /// <summary>
+    /// Ensures a file exists in the storage and returns pre-signed URL of the file 
+    /// </summary>
+    /// <param name="fileName">Name of a file that exists in the storage</param>
+    /// <param name="expiration">Time of url expiration</param>
+    /// <param name="cancellation">Cancellation token</param>
+    /// <returns>Returns <see cref="string"/> with pre-signed URL of file or <b>null</b> if there isn't a file</returns>
+    /// <exception cref="HttpRequestException">Connection problems or other unexpected <see cref="HttpStatusCode"/></exception>
+    Task<string?> GetFileUrl(string fileName, TimeSpan expiration, CancellationToken cancellation);
+
+    /// <summary>
+    /// Returns a list of files (only 1000 first) by <paramref name="prefix"/>  
+    /// </summary>
+    /// <param name="prefix">Prefix of file names</param>
+    /// <param name="cancellation">Cancellation token</param>
+    /// <returns>Async collection of file names</returns>
+    IAsyncEnumerable<string> List(string? prefix, CancellationToken cancellation);
+
+    Task<bool> MultipartAbort(string fileName, string uploadId, CancellationToken cancellation);
+
+    /// <summary>
+    /// Completes multipart upload operation
+    /// </summary>
+    /// <param name="fileName">Name of file</param>
+    /// <param name="uploadId">Identity of upload</param>
+    /// <param name="partTags">Tags of parts, sorted by partNumber</param>
+    /// <param name="tagsCount">Count of parts in <paramref name="partTags"/></param>
+    /// <param name="cancellation">Cancellation token</param>
+    /// <returns>Returns <b>true</b> if file has been uploaded or <b>false</b> if not</returns>
+    Task<bool> MultipartComplete(
+        string fileName, string uploadId, string[] partTags, int tagsCount,
+        CancellationToken cancellation);
+
+    Task<string> MultipartStart(string fileName, string fileType, CancellationToken cancellation);
+
+    Task<string?> MultipartUpload(
+        string fileName, string uploadId,
+        int partNumber, byte[] partData, int partSize,
+        CancellationToken cancellation);
+
+    Task<bool> PutFile(string fileName, Stream data, string contentType, CancellationToken cancellation);
+
+    Task<bool> PutFile(
+        string fileName, byte[] data, string contentType,
+        CancellationToken cancellation);
+
+    Task<bool> PutFile(
+        string fileName, byte[] data, int offset, int count, string contentType,
+        CancellationToken cancellation);
+
+    Task<bool> PutFileMultipart(
+        string fileName, Stream data, string contentType,
+        CancellationToken cancellation);
+
+    Task<bool> PutFileMultipart(
+        string fileName, Stream data, string contentType, int partSize,
+        CancellationToken cancellation);
+
+    /// <summary>
+    /// Uploads a file data to the storage
+    /// </summary>
+    /// <remarks>If length of <paramref name="data"/> greater 5 Mb then will use multipart method of upload instead just direct upload</remarks>
+    /// <param name="fileName">Name of data</param>
+    /// <param name="data">Stream with file data</param>
+    /// <param name="contentType">Type of file data in MIME format</param>
+    /// <param name="cancellation">Cancellation token</param>
+    /// <returns>Returns <b>true</b> if file has been uploaded or <b>false</b> if not</returns>
+    /// <exception cref="HttpRequestException">Connection problems or other unexpected <see cref="HttpStatusCode"/></exception>
+    Task<bool> UploadFile(string fileName, Stream data, string contentType, CancellationToken cancellation);
+}
--- src/Storage/StorageClient.cs
+++ src/Storage/StorageClient.cs
@@ -12,7 +12,7 @@ namespace Storage;
 
 [DebuggerDisplay("Client for '{_bucket}'")]
 [SuppressMessage("ReSharper", "SwitchStatementHandlesSomeKnownEnumValuesWithDefault")]
-public sealed class StorageClient : IDisposable
+public sealed class StorageClient : IStorageClient
 {
     private const int DefaultPartSize = 5 * 1024 * 1024; // 5 Mb
 
@@ -49,13 +49,6 @@ public sealed class StorageClient : IDisposable
         _useHttp2 = settings.UseHttp2;
     }
 
-    /// <summary>
-    /// Builds pre-signed file url
-    /// </summary>
-    /// <remarks>This method will not check if the file exists or not: use <see cref="GetFileUrl"/> for check existing of file</remarks>
-    /// <param name="fileName">Name of file</param>
-    /// <param name="expiration">Time of url expiration</param>
-    /// <returns>Pre-signed URL of the file</returns>
     public string BuildFileUrl(string fileName, TimeSpan expiration)
     {
         var now = DateTime.UtcNow;
@@ -131,13 +124,6 @@ public sealed class StorageClient : IDisposable
         }
     }
 
-    /// <summary>
-    /// Deletes the file from storage
-    /// </summary>
-    /// <param name="fileName">Name of the file to be deleted</param>
-    /// <param name="cancellation">Cancellation token</param>
-    /// <remarks>Storage sends the same response in the following cases: the file was indeed deleted, the file did not exist</remarks>
-    /// <exception cref="HttpRequestException">Connection problems or status code isn't <see cref="HttpStatusCode.NoContent"/></exception>
     public async Task DeleteFile(string fileName, CancellationToken cancellation)
     {
         HttpResponseMessage response;
@@ -172,13 +158,6 @@ public sealed class StorageClient : IDisposable
         }
     }
 
-    /// <summary>
-    /// Gets file data from the storage
-    /// </summary>
-    /// <param name="fileName">File name than will be received</param>
-    /// <param name="cancellation">Cancellation token</param>
-    /// <returns>Wrapper around <see cref="HttpResponseMessage"/> with the data of the file from storage</returns>
-    /// <exception cref="HttpRequestException">Connection problems or status code isn't isn't <see cref="HttpStatusCode.OK"/> or <see cref="HttpStatusCode.NotFound"/></exception>
     public async Task<StorageFile> GetFile(string fileName, CancellationToken cancellation)
     {
         HttpResponseMessage response;
@@ -200,13 +179,6 @@ public sealed class StorageClient : IDisposable
         }
     }
 
-    /// <summary>
-    /// Gets file stream from the storage
-    /// </summary>
-    /// <param name="fileName">File name than will be received</param>
-    /// <param name="cancellation">Cancellation token</param>
-    /// <returns>Stream from <see cref="HttpResponseMessage"/> with the data of the file from storage</returns>
-    /// <exception cref="HttpRequestException">Connection problems or status code isn't <see cref="HttpStatusCode.OK"/> or <see cref="HttpStatusCode.NotFound"/></exception>
     public async Task<Stream> GetFileStream(string fileName, CancellationToken cancellation)
     {
         HttpResponseMessage response;
@@ -228,14 +200,6 @@ public sealed class StorageClient : IDisposable
         }
     }
 
-    /// <summary>
-    /// Ensures a file exists in the storage and returns pre-signed URL of the file 
-    /// </summary>
-    /// <param name="fileName">Name of a file that exists in the storage</param>
-    /// <param name="expiration">Time of url expiration</param>
-    /// <param name="cancellation">Cancellation token</param>
-    /// <returns>Returns <see cref="string"/> with pre-signed URL of file or <b>null</b> if there isn't a file</returns>
-    /// <exception cref="HttpRequestException">Connection problems or other unexpected <see cref="HttpStatusCode"/></exception>
     public async Task<string?> GetFileUrl(string fileName, TimeSpan expiration, CancellationToken cancellation)
     {
         return await FileExists(fileName, cancellation).ConfigureAwait(false)
@@ -243,15 +207,7 @@ public sealed class StorageClient : IDisposable
             : null;
     }
 
-    /// <summary>
-    /// Returns a list of files (only 1000 first) by <paramref name="prefix"/>  
-    /// </summary>
-    /// <param name="prefix">Prefix of file names</param>
-    /// <param name="cancellation">Cancellation token</param>
-    /// <returns>Async collection of file names</returns>
-    public async IAsyncEnumerable<string> List(
-        string? prefix,
-        [EnumeratorCancellation] CancellationToken cancellation)
+    public async IAsyncEnumerable<string> List(string? prefix, [EnumeratorCancellation] CancellationToken cancellation)
     {
         var url = string.IsNullOrEmpty(prefix)
             ? $"{_bucket}?list-type=2"
@@ -308,15 +264,6 @@ public sealed class StorageClient : IDisposable
         return result;
     }
 
-    /// <summary>
-    /// Completes multipart upload operation
-    /// </summary>
-    /// <param name="fileName">Name of file</param>
-    /// <param name="uploadId">Identity of upload</param>
-    /// <param name="partTags">Tags of parts, sorted by partNumber</param>
-    /// <param name="tagsCount">Count of parts in <paramref name="partTags"/></param>
-    /// <param name="cancellation">Cancellation token</param>
-    /// <returns>Returns <b>true</b> if file has been uploaded or <b>false</b> if not</returns>
     public async Task<bool> MultipartComplete(
         string fileName, string uploadId, string[] partTags, int tagsCount,
         CancellationToken cancellation)
@@ -545,16 +492,6 @@ public sealed class StorageClient : IDisposable
         return completeResult;
     }
 
-    /// <summary>
-    /// Uploads a file data to the storage
-    /// </summary>
-    /// <remarks>If length of <paramref name="data"/> greater 5 Mb then will use multipart method of upload instead just direct upload</remarks>
-    /// <param name="fileName">Name of data</param>
-    /// <param name="data">Stream with file data</param>
-    /// <param name="contentType">Type of file data in MIME format</param>
-    /// <param name="cancellation">Cancellation token</param>
-    /// <returns>Returns <b>true</b> if file has been uploaded or <b>false</b> if not</returns>
-    /// <exception cref="HttpRequestException">Connection problems or other unexpected <see cref="HttpStatusCode"/></exception>
     public Task<bool> UploadFile(string fileName, Stream data, string contentType, CancellationToken cancellation)
     {
         return data.Length is > 0 and > DefaultPartSize
