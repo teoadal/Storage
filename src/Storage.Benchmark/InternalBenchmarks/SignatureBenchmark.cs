4e754e9604a0ce21c4bb9c58913f543f38990e43
Ihor Volokhovych <igorvolokhovych@gmail.com>
2023-07-25 06:11:54 +0300

    feat: refactored SignatureBenchmark.cs

--- src/Storage.Benchmark/InternalBenchmarks/SignatureBenchmark.cs
+++ src/Storage.Benchmark/InternalBenchmarks/SignatureBenchmark.cs
@@ -6,40 +6,43 @@ using Storage.Utils;
 namespace Storage.Benchmark.InternalBenchmarks;
 
 [SimpleJob(RuntimeMoniker.Net70)]
-[MeanColumn, MemoryDiagnoser]
+[MeanColumn]
+[MemoryDiagnoser]
 public class SignatureBenchmark
 {
-    [Benchmark]
-    public int Content() => _signature
-        .Calculate(_request, _payloadHash, _headers, _now)
-        .Length;
+	private string[] _headers = null!;
+	private DateTime _now;
+	private HttpRequestMessage _request = null!;
+	private string _payloadHash = null!;
+	private Signature _signature = null!;
 
-    [Benchmark]
-    public int Url() => _signature
-        .Calculate("http://subdomain-name.company-name.com/controller/method?arg=eto-arg", _now)
-        .Length;
+	[GlobalSetup]
+	public void Config()
+	{
+		var config = BenchmarkHelper.ReadConfiguration();
+		var data = BenchmarkHelper.ReadBigArray(config);
+		var settings = BenchmarkHelper.ReadSettings(config);
 
-    #region Configuration
+		_headers = new[] { "host", "x-amz-content-sha256", "x-amz-date" };
+		_now = DateTime.UtcNow;
+		_request = new HttpRequestMessage(HttpMethod.Post, "http://company-name.com/controller");
+		_payloadHash = HashHelper.GetPayloadHash(data);
+		_signature = new Signature(settings.SecretKey, settings.Region, settings.Service);
+	}
 
-    private string[] _headers = null!;
-    private DateTime _now;
-    private HttpRequestMessage _request = null!;
-    private string _payloadHash = null!;
-    private Signature _signature = null!;
+	[Benchmark]
+	public int Content()
+	{
+		return _signature
+			.Calculate(_request, _payloadHash, _headers, _now)
+			.Length;
+	}
 
-    [GlobalSetup]
-    public void Config()
-    {
-        var config = BenchmarkHelper.ReadConfiguration();
-        var data = BenchmarkHelper.ReadBigArray(config);
-        var settings = BenchmarkHelper.ReadSettings(config);
-
-        _headers = new[] {"host", "x-amz-content-sha256", "x-amz-date"};
-        _now = DateTime.UtcNow;
-        _request = new HttpRequestMessage(HttpMethod.Post, "http://company-name.com/controller");
-        _payloadHash = HashHelper.GetPayloadHash(data);
-        _signature = new Signature(settings.SecretKey, settings.Region, settings.Service);
-    }
-
-    #endregion
+	[Benchmark]
+	public int Url()
+	{
+		return _signature
+			.Calculate("http://subdomain-name.company-name.com/controller/method?arg=eto-arg", _now)
+			.Length;
+	}
 }
